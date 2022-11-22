35166963cb1edbe1b00f718a56454ad829a60490
Rene Santiago <rsj510@gmail.com>
2022-11-22 18:18:30 -0500

    added edit route

--- server.js
+++ server.js
@@ -21,6 +21,36 @@ app.get('/pokemon', (req, res) => {
     })
 })
 
+// New
+app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs', {
+        pokemon: pokemon,
+    })
+})
+
+// Destroy
+// app.delete('pokemon/:id', (req, res) => {
+//     const id = req.params.id
+//     pokemon.findIndex() ={
+
+//     }
+// })
+
+// Update
+
+// Create
+app.post('/pokemon', (req, res) => {
+    pokemon.push(req.body)
+    res.redirect('/pokemon')
+})
+
+// Edit
+app.get('/pokemon/:id/edit', (req, res) => {
+    res.render('edit.ejs', {
+        pokemon: pokemon[req.params.id]
+    })
+})
+
 // Show
 app.get('/pokemon/:id', (req, res) => {
     res.render('show.ejs', {
